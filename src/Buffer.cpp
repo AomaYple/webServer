b76354361d8394b95be025daafce61da95244971
AomaYple <2890599214@qq.com>
2023-04-07 09:17:20 +0800

    增加Buffer类

--- src/Buffer.cpp
+++ src/Buffer.cpp
@@ -1,5 +1,65 @@
-//
-// Created by 28905 on 2023/4/7.
-//
-
 #include "Buffer.h"
+
+using std::string_view, std::pair;
+
+Buffer::Buffer(unsigned int size) : self(size), start(0), end(0) {}
+
+Buffer::Buffer(Buffer &&buffer) noexcept : self(std::move(buffer.self)), start(buffer.start), end(buffer.end) {
+    buffer.start = buffer.end = 0;
+}
+
+auto Buffer::operator=(Buffer &&buffer) noexcept -> Buffer & {
+    if (this != &buffer) {
+        this->self = std::move(buffer.self);
+        this->start = buffer.start;
+        this->end = buffer.end;
+        buffer.start = buffer.end = 0;
+    }
+    return *this;
+}
+
+auto Buffer::write(const string_view &data) -> void {
+    if (data.size() > this->self.size() - this->end && this->start > 0) {
+        std::copy(this->self.begin() + this->start, this->self.begin() + this->end, this->self.begin());
+        this->end -= this->start;
+        this->start = 0;
+    }
+
+    if (data.size() > this->self.size() - this->end)
+        this->self.resize((data.size() + this->end - this->start) * 2);
+
+    std::copy(data.begin(), data.end(), this->self.begin() + this->end);
+    this->end += data.size();
+}
+
+auto Buffer::read() -> string_view {
+    return {this->self.begin() + this->start, this->self.begin() + this->end};
+}
+
+auto Buffer::writableData() -> pair<char *, unsigned int> {
+    return {this->self.data() + this->end, this->self.size() - this->end};
+}
+
+auto Buffer::adjustWrite(unsigned int size) -> void {
+    this->end += size;
+
+    if (this->end == this->self.size() && this->start > 0) {
+        std::copy(this->self.begin() + this->start, this->self.begin() + this->end, this->self.begin());
+        this->end -= this->start;
+        this->start = 0;
+    }
+
+    if (this->end == this->self.size())
+        this->self.resize(this->self.size() * 2);
+}
+
+auto Buffer::adjustRead(unsigned int size) -> void {
+    this->start += size;
+
+    if (this->start == this->end)
+        this->start = this->end = 0;
+}
+
+auto Buffer::empty() const -> bool {
+    return this->start == this->end;
+}
