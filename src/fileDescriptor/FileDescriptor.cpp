745c3aab34d3f4592409abb7d19e74f2efdea498
AomaYple <2890599214@qq.com>
2024-01-15 10:12:34 +0800

    小修改

--- src/fileDescriptor/FileDescriptor.cpp
+++ src/fileDescriptor/FileDescriptor.cpp
@@ -2,18 +2,16 @@
 
 #include "../ring/Submission.hpp"
 
-#include <utility>
-
 FileDescriptor::FileDescriptor(int fileDescriptor, std::shared_ptr<Ring> ring) noexcept
     : fileDescriptor{fileDescriptor}, ring{std::move(ring)} {}
 
 FileDescriptor::FileDescriptor(FileDescriptor &&other) noexcept
-    : fileDescriptor{std::exchange(other.fileDescriptor, -1)}, ring{std::move(other.ring)} {}
+    : fileDescriptor{other.fileDescriptor}, ring{std::move(other.ring)} {}
 
 auto FileDescriptor::operator=(FileDescriptor &&other) noexcept -> FileDescriptor & {
     this->close();
 
-    this->fileDescriptor = std::exchange(other.fileDescriptor, -1);
+    this->fileDescriptor = other.fileDescriptor;
     this->ring = std::move(other.ring);
 
     return *this;
@@ -26,7 +24,7 @@ auto FileDescriptor::getFileDescriptor() const noexcept -> int { return this->fi
 auto FileDescriptor::getRing() const noexcept -> std::shared_ptr<Ring> { return this->ring; }
 
 auto FileDescriptor::close() const -> void {
-    if (this->fileDescriptor != -1) {
+    if (this->ring != nullptr) {
         const Submission submission{Event{Event::Type::close, this->fileDescriptor}, 0, Submission::CloseParameters{}};
         this->ring->submit(submission);
     }
